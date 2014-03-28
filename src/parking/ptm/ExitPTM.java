188ec791be96f6a5435b4a39a3b05d86da76035c
felipe.acorsi@gmail.com <felipe.acorsi@gmail.com@52a1b7aa-0434-0c88-9418-51db6748b858>
2014-03-28 01:13:55 +0000

    Main classes created.

---
+++ src/parking/ptm/ExitPTM.java
@@ -0,0 +1,23 @@
+package parking.ptm;
+
+import javax.smartcardio.CardTerminal;
+
+public class ExitPTM extends ParkingTicketMachine{
+
+	public ExitPTM(CardTerminal cardTerminal) {
+		super(cardTerminal);
+	}
+
+	@Override
+	public void Initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
