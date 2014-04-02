2670a1e6d42094644eaedaf7f753ea3995d896cb
felipe.acorsi@gmail.com <felipe.acorsi@gmail.com@52a1b7aa-0434-0c88-9418-51db6748b858>
2014-04-02 23:51:56 +0000

    Protocol implementation started. First communications tests with android application

---
+++ src/parking/protocol/Protocol.java
@@ -0,0 +1,41 @@
+package parking.protocol;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.commons.codec.binary.Hex;
+
+public abstract class Protocol implements IProtocol{
+
+	private Logger log;
+	
+	public Protocol ()
+	{
+		log = LoggerFactory.getLogger(getClass());
+	}
+	
+	public void IdentifyCommand (byte[] dataIn)
+	{
+		if (dataIn.length < 4) return;
+		
+		//Check command start
+		if (dataIn[0] != con_start)
+		{
+			log.info("Wrong start command");
+		}
+		
+		switch (dataIn[1])
+		{
+		case con_cmd_get:
+			this.getUserId();
+			break;
+		
+		default:
+			log.info("Unknow command: " + Hex.encodeHexString(dataIn));
+			break;
+		}
+		
+	}
+	
+	protected abstract void getUserId ();
+
+}
--- src/parking/ptm/nfc/IsoDepTamaCommunicator.java
+++ src/parking/ptm/nfc/IsoDepTamaCommunicator.java
@@ -1,6 +1,7 @@
 package parking.ptm.nfc;
 
 import java.io.IOException;
+import java.io.SyncFailedException;
 
 import org.nfctools.io.ByteArrayReader;
 import org.nfctools.io.ByteArrayWriter;
@@ -12,13 +13,14 @@ import org.nfctools.spi.tama.response.InListPassiveTargetResp;
 import org.nfctools.utils.NfcUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import parking.protocol.SWProtocol;
 
 public class IsoDepTamaCommunicator extends AbstractTamaCommunicator {
 
 	private Logger log = LoggerFactory.getLogger(getClass());
 	private int messageCounter = 0;
 	private static final byte[] CLA_INS_P1_P2 = { 0x00, (byte)0xA4, 0x04, 0x00 };
-	private static final byte[] AID_ANDROID = { (byte)0xF0, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
+	private static final byte[] AID_ANDROID = { (byte)0xF0, 0x01, 0x02, 0x03, 0x04, 0x05, 0x07 };
 
 	public IsoDepTamaCommunicator(ByteArrayReader reader, ByteArrayWriter writer) {
 		super(reader, writer);
@@ -41,14 +43,14 @@ public class IsoDepTamaCommunicator extends AbstractTamaCommunicator {
 				log.info("TargetData: " + NfcUtils.convertBinToASCII(inListPassiveTargetResp.getTargetData()));
 				if (inListPassiveTargetResp.isIsoDepSupported()) {
 					log.info("IsoDep Supported");
-					byte[] selectAidApdu = createSelectAidApdu(AID_ANDROID);
-					DataExchangeResp resp = sendMessage(new DataExchangeReq(inListPassiveTargetResp.getTargetId(),
-							false, selectAidApdu, 0, selectAidApdu.length));
-					String dataIn = new String(resp.getDataOut());
-					log.info("Received: " + dataIn);
-					if (dataIn.startsWith("Hello")) {
-						exchangeData(inListPassiveTargetResp);
-					}
+					
+					log.info("Authenticating App ID");
+					authApplicationID(inListPassiveTargetResp);
+					log.info("App ID authenticated");
+					
+					log.info("Authenticating user ID");
+					int uId = getUserId (inListPassiveTargetResp);
+					log.info("User ID: " + uId);
 				}
 				else {
 					log.info("IsoDep NOT Supported");
@@ -66,6 +68,36 @@ public class IsoDepTamaCommunicator extends AbstractTamaCommunicator {
 		}
 	}
 
+	private void authApplicationID(InListPassiveTargetResp inListPassiveTargetResp) throws IOException
+	{
+		byte[] selectAidApdu = createSelectAidApdu(AID_ANDROID);
+		DataExchangeResp resp = sendMessage(new DataExchangeReq(inListPassiveTargetResp.getTargetId(),
+				false, selectAidApdu, 0, selectAidApdu.length));
+		String dataIn = new String(resp.getDataOut());
+		
+		if (!dataIn.equals("OK"))
+		{
+			throw new SyncFailedException("Authentication not received");
+		}
+	}
+	
+	private int getUserId(InListPassiveTargetResp inListPassiveTargetResp) throws IOException 
+	{
+		DataExchangeResp resp;
+		String dataIn;
+		
+		byte[] dataOut = SWProtocol.getUserIDCommand();
+		resp = sendMessage(new DataExchangeReq(inListPassiveTargetResp.getTargetId(), false, dataOut, 0,
+				dataOut.length));
+		
+		dataIn = new String(resp.getDataOut());		
+		
+		return 0;
+	}
+	
+	
+	
+	
 	private void exchangeData(InListPassiveTargetResp inListPassiveTargetResp) throws IOException {
 		DataExchangeResp resp;
 		String dataIn;
